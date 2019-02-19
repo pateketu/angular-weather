6bfb6abe1dacdba1493fe0eb560425af41dfb22a
Ketul Patel <pateketu@gmail.com>
2019-02-19 00:30:49 +0000

    attempt to do e2e test

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from './app.po';
+import { element, by } from 'protractor';
 
 describe('angular-weather App', () => {
   let page: AppPage;
@@ -7,7 +8,20 @@ describe('angular-weather App', () => {
     page = new AppPage();
   });
 
-  it('e2e are running empty, please implement', () => {
-    expect(page);
+  it('searching for a city forecast renders forecast table', () => {
+    page.navigateTo();
+    element(by.id('city')).sendKeys('Seattle');
+    element(by.className('search')).click();
+    /* Not sure how to wait for call to come back */
+
+    /* Apologis not going to have time to dig into
+    Protractor Syntax, */
+
+    /* In Our current project we diteched Protractor after intially attempting to use it and
+    instead we are using Cypress for end-to-end test and have found it much
+    for developer friendly in terms of productivity and it's really fast
+    https://hackernoon.com/cypress-io-vs-protractor-e2e-testing-battle-d124ece91dc7
+    https://techblog.fexcofts.com/2018/09/24/end-to-end-e2e-angular-testing-protractor-vs-cypress/
+    */
   });
 });
